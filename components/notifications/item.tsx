bf17fc4ab9489046da64b2482694531982677d44
shovoncse <shovondas26@gmail.com>
2023-12-12 15:43:38 +0200

    :construction: notification design

---
+++ components/notifications/item.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+import { Loader, Package, PackageCheck, PackageSearch, Truck } from "lucide-react";
+interface NotificationItemProps {
+  notification: any;
+}
+const NotificatioItem: React.FC<NotificationItemProps> = ({ notification }) => {
+
+  // const changeNotification = async (notification: any) => {
+  //   console.log(notification);
+  // };
+
+  // color based on status
+  const color = (status: string) => {
+    switch (status) {
+      case "delivered":
+        return "bg-[#FFECF5]";
+      case "received":
+        return "bg-[#D7FAD1]";
+      default:
+        return "bg-slate-100";
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        "flex flex-row gap-2 items-center hover:cursor-pointer hover:bg-[#fedbc28e] p-2 px-3 border rounded-sm", color(notification?.status))}
+      // onClick={() => {
+      //   changeNotification();
+      // }}
+    >
+      <p className="text-center">
+        {notification?.status === "delivered" ? <Truck /> : ""}
+        {notification?.status === "received" ? <PackageCheck /> : ""}
+        {notification?.status === "picked" ? <Package /> : ""}
+        {notification?.status === "sent" ? <PackageSearch /> : ""}
+        {notification?.status === "reserved" ? <Loader /> : ""}
+      </p>
+      <p className="text-sm">
+        {notification?.title}
+        <br />
+      </p>
+      <p className="text-xs text-[#686868] ml-auto">{notification?.time}</p>
+    </div>
+  );
+};
+
+export default NotificatioItem;
--- components/notifications/notifications.tsx
+++ components/notifications/notifications.tsx
@@ -1,9 +1,73 @@
+import NotificatioItem from "./item";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const Notifications = () => {
-  return ( 
-    <div>
-      Notifications
+  const data = [
+    {
+      id: 1,
+      title: "Your package has been delivered",
+      time: "2 hours ago",
+      status: "delivered",
+    },
+    {
+      id: 2,
+      title: "Your package has been received",
+      time: "2 hours ago",
+      status: "received",
+    },
+    {
+      id: 3,
+      title: "Your package has been picked up",
+      time: "2 hours ago",
+      status: "picked",
+    },
+    {
+      id: 4,
+      title: "Your package has been sent",
+      time: "2 hours ago",
+      status: "sent",
+    },
+    {
+      id: 5,
+      title: "Your package has been reserved",
+      time: "2 hours ago",
+      status: "reserved",
+    },
+  ];
+
+  return (
+    <div className="py-4 px-4">
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select Category" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Notifications</SelectLabel>
+            <SelectItem className="cursor-pointer" value="all">All</SelectItem>
+            <SelectItem className="cursor-pointer" value="delivered">Delivered</SelectItem>
+            <SelectItem className="cursor-pointer" value="received">Received</SelectItem>
+            <SelectItem className="cursor-pointer" value="picked">Picked</SelectItem>
+            <SelectItem className="cursor-pointer" value="sent">Sent</SelectItem>
+            <SelectItem className="cursor-pointer" value="reserved">Reserved</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <div className="flex flex-col gap-2 pt-4">
+        {data.map((notification) => (
+          <NotificatioItem key={notification.id} notification={notification} />
+        ))}
+      </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Notifications;
